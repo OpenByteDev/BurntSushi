dcc1a68f9dcfa09d1fb57075eb9e14ee7ed297bc
OpenByte <development.openbyte@gmail.com>
2021-04-21 21:53:36 +0200

    Update filter list

--- InjectionPayload/RequestFilter.cs
+++ InjectionPayload/RequestFilter.cs
@@ -28,6 +28,7 @@ namespace InjectionPayload {
             "i.scdn.co", // cover art
             "lineup-images.scdn.co", // playlists lineup images
             "merch-img.scdn.co", // merch images
+            "misc.scdn.co", // miscellaneous images
             "mosaic.scdn.co", // playlist mosaic images
             "newjams-images.scdn.co", // release radar images
             "o.scdn.co", // cover art
@@ -45,17 +46,26 @@ namespace InjectionPayload {
             "hwcdn.libsyn.com", // podcasts
             "traffic.libsyn.com", // podcasts
             "api*-desktop.musixmatch.com", // lyrics (genius-spicetify)
+            "*.podbean.com", // podcasts
+            "dts.podtrac.com", // podcasts
             "www.podtrac.com", // podcasts
             "audio.simplecast.com", // podcasts
             "media.simplecast.com", // podcasts
             "ap.spotify.com", // audio (access point)
             "*.ap.spotify.com", // resolved access points
+            "api.spotify.com", // client APIs
+            "api-partner.spotify.com", // album/artist pages
+            "xpui.app.spotify.com", // user interface
             "apresolve.spotify.com", // access point resolving
-            "dealer.spotify.com", // player activity (websocket)
+            "clienttoken.spotify.com", // login
+            "*dealer.spotify.com", // websocket connections
             "image-upload*.spotify.com", // image uploading
             "login*.spotify.com", // login
-            "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
+            "*-spclient.spotify.com", // client APIs
+            "spclient.wg.spotify.com", // client APIs, ads/tracking (blocked in blacklist)
             "audio-fa.spotifycdn.com", // audio
+            "seed-mix-image.spotifycdn.com", // mix images
+            "download.ted.com", // podcasts
             "dcs*.megaphone.fm", // podcasts
             "traffic.megaphone.fm", // podcasts
             "audio-ak-spotify-com.akamaized.net", // audio
@@ -64,9 +74,8 @@ namespace InjectionPayload {
             "audio4-ak.spotify.com.edgesuite.net", // audio
             "scontent*.fbcdn.net", // Facebook profile images
             "audio-sp-*.spotifycdn.net", // audio
-
-            // added
-            // "*-accesspoint-*.ap.spotify.com"
+            "dovetail.prxu.org", // podcasts
+            "dovetail-cdn.prxu.org", // podcasts
         };
     }
 }
